6fdb2a3b3e439ce430e0e0b35e088f4e1db1defa
alexandergrill <grillalexander0@gmail.com>
2021-02-27 21:02:43 +0100

    create project

---
+++ src/src/client/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * author:  Alexander Grill
+ * file:    main.cpp
+ * project: mapreduce
+ * date:    27.02.2021
+*/
+
+
+
+
+int main(){
+    return 0;
+}
---
+++ src/src/server/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * author:  Alexander Grill
+ * file:    main.cpp
+ * project: mapreduce
+ * date:    27.02.2021
+*/
+
+
+int main(){
+    return 0;
+}
