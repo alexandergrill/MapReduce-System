ac3d9e2dac1cfa2c886dc8272319416e394ae845
alexandergrill <grillalexander0@gmail.com>
2021-03-09 16:05:34 +0100

    client connected to Server

--- source/src/client/main.cpp
+++ source/src/client/main.cpp
@@ -15,18 +15,35 @@
 #include <CLI11/CLI11.hpp>
 
 /*int argc, char* argv[]*/
+//c->WriteIntoFile(500000, "../src/client/clientfile.txt");
+//c->Map("../src/client/clientfile.txt");
 
 #include <iostream>
 
 using namespace std;
+using namespace asio::ip;
 
 int main(){
-    string ipadress = "127.0.0.1a";
-    string port = "111";
+    string ipadress = "127.0.0.1";
+    string port = "1113";
+    string test = "HULLOWorld";
 
     Client* c = Client::GetClient(ipadress, port);
     if(c != nullptr){
-        //c->WriteIntoFile(500000, "../src/client/clientfile.txt");
-        //c->Map("../src/client/clientfile.txt");
+        char* my_ip = &ipadress[0];
+        char* my_port = &port[0];
+
+        tcp::iostream tcpconnection{my_ip, my_port};
+
+        if(tcpconnection){
+            try{
+                tcpconnection << test << endl;
+                //getline(tcpconnection, test);
+            }
+            catch(...){
+                cerr << "Error" << endl;
+            }
+        } 
+        tcpconnection.close();  
     }
 }
--- source/src/server/main.cpp
+++ source/src/server/main.cpp
@@ -6,6 +6,39 @@
 */
 
 
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+#include <asio.hpp>
+#include <rang/rang.hpp>
+#include <CLI11/CLI11.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace asio::ip;
+
+
+
 int main(){
-    return 0;
+    unsigned short port = 1113;
+    tcp::endpoint ep{tcp::v4(), port};
+    asio::io_context cox;
+    tcp::acceptor ap{cox, ep};
+
+    std::map<std::string, int> test1;
+    while(true){
+        ap.listen();
+        cout << "Server is listening" << endl;
+        try{
+            tcp::iostream strm{ap.accept()};
+            string data = "";
+            strm >> data;
+            cout << "DataTest " << data << endl;
+        }
+        catch(...){
+            cerr << "Error" << endl;
+        }
+    }
 }
